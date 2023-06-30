f74dffca581e13c1e2f679d418d494ac7f5b8475
maazkh10 <120632580+maazkh10@users.noreply.github.com>
2023-07-01 03:09:01 +0530

    addedd

---
+++ wen.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  $(document).ready(function(){
+
+    $('#itemslider').carousel({ interval: 3000 });
+    
+    $('.carousel-showmanymoveone .item').each(function(){
+    var itemToClone = $(this);
+    
+    for (var i=1;i<6;i++) {
+    itemToClone = itemToClone.next();
+    
+    if (!itemToClone.length) {
+    itemToClone = $(this).siblings(':first');
+    }
+    
+    itemToClone.children(':first-child').clone()
+    .addClass("cloneditem-"+(i))
+    .appendTo($(this));
+    }
+    });
+    });
+    
